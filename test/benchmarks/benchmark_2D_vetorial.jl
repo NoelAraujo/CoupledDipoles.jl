20e85814d22975997ea06d319470b7c528572ae7
Noel <noelaraujo@live.com>
2021-07-15 15:03:14 -0300

    Add initial benchmakrs
    - created runtests.jl with some exact values of interaction matrix
     - added 2D and 3D vectorial files for future use

---
+++ test/benchmarks/benchmark_2D_vetorial.jl
@@ -0,0 +1,93 @@
+#=
+    For this particular positions
+        r = [1 2; 3 5]
+    the expression below are exact solutions.
+
+    Check sagemath notebook for the proof.
+=#
+N = 2
+r = [1 2; 3 5]
+    
+using SpecialFunctions, LinearAlgebra
+H_0 = [1 besselh.(0,sqrt(13)); besselh.(0,sqrt(13)) 1]
+H_m = [0 exp(-2im*atan(3,2))*besselh.(2,sqrt(13));
+            exp(-2im*atan(3,2))*besselh.(2,sqrt(13)) 0]
+
+H_p = [0 exp(+2im*atan(3,2))*besselh.(2,sqrt(13));
+    exp(+2im*atan(3,2))*besselh.(2,sqrt(13)) 0]
+
+K_exact = [H_0 H_p; H_m H_0]
+
+
+
+
+#=
+        Creating matrices individually
+=#
+let
+    N = 2
+    r = [1 2; 3 5]
+    Dx = [r[j,1] - r[l,1] for j=1:N, l=1:N]
+    Dy = [r[j,2] - r[l,2] for j=1:N, l=1:N]
+    r_jl = sqrt.( Dx.^2 + Dy.^2  )
+    φ_jl = atan.(Dy, Dx)
+    
+    #Hankel functions
+    H0 = besselh.(0,r_jl)
+    H0[findall(  isnan.(H0) )] .= 0.0
+    H0[diagind(H0)] .= 1
+
+    Hp = exp.(+2*1im*φ_jl).*besselh.(2,r_jl)
+    Hp[diagind(Hp)] .= 0
+    Hm = exp.(-2*1im*φ_jl).*besselh.(2,r_jl)
+    Hm[diagind(Hm)] .= 0
+
+    K = [H0 Hp; Hm H0]
+    K .≈ K_exact
+end
+
+
+
+
+#=
+        Creating matrices generically
+=#
+let
+    N = 2
+    r = [1 2; 3 5]
+    δ(x,y) = Int(==(x,y))
+    Dx = [r[j,1] - r[l,1] for j=1:N, l=1:N]
+    Dy = [r[j,2] - r[l,2] for j=1:N, l=1:N]
+    r_jl = sqrt.( Dx.^2 + Dy.^2  )
+    φ_jl = atan.(Dy, Dx)
+
+    H0 = besselh.(0,r_jl)
+    H2 = besselh.(2,r_jl)
+
+    A = []
+    α_range = β_range = [-1, +1]
+    for (α_idx, α) ∈ enumerate(α_range)
+        B = []
+        for (β_idx, β) ∈ enumerate(β_range)
+            term1 = I(N)*δ(α,β)
+            term2 = ones(N, N) - I(N)
+            
+            term3 = δ(α,β)*H0
+            term3[findall(isnan.(term3))] .= 0
+            term3[findall(isinf.(term3))] .= 0
+
+            term4 = (1 - δ(α,β))*exp.(2*α*im*φ_jl).*H2
+            term4[findall(isnan.(term4))] .= 0
+            
+            temp =  term1 + term2.*(term3 + term4 )
+
+            push!(B, temp)
+        end
+        push!(A, vcat(B[1:length(β_range)]...)  )
+    end
+
+    K = hcat(A[1:length(α_range)]...)
+    K .≈ K_exact
+end
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,52 @@ using CoupledDipole
 using Test
 
 @testset "CoupledDipole.jl" begin
-    # Write your tests here.
+    @testset "3D scalar: 2 atoms" begin
+        r =[1 2 0;
+            1 0 1.0]
+        atoms =  Cube(r, 10)
+        laser = PlaneWave_3D(:z, 1e-6, 1)
+        problem = ScalarProblem(atoms, laser)
+        S = get_spectrum(problem)
+        Γₙ = sort(-real.(S.λ))
+        ωₙ = sort(imag.(S.λ))
+    
+        Γₙ_ans = [0.675922453937850, 0.324077546062151]
+        ωₙ_ans = [0.861973588757495, 1.13802641124251]
+        @test all(sort(ωₙ_ans) .≈ sort(ωₙ))
+        @test all(sort(Γₙ_ans) .≈ sort(Γₙ))
+        
+        βₛₛ = get_steady_state(problem)
+        βₛₛ_ans = [0.000695478188875652-0.000416450295287958*im,
+                   0.000554066244758147+0.000208376732488666*im ]
+        @test all(βₛₛ_ans .≈ βₛₛ)
+    end
+    @testset "3D scalar: 3 atoms" begin
+        r =[ 0 0 0;
+             1 1 1; 
+            -1 1 0.5]
+        atoms =  Cube(r, 10)
+        laser = PlaneWave_3D(:z, 1e-6, 0)
+        problem = ScalarProblem(atoms, laser)
+        S = get_spectrum(problem)
+        Γₙ = sort(-real.(S.λ))
+        ωₙ = sort(imag.(S.λ))
+    
+        Γₙ_ans = [0.293248907595325, 0.15447403278592, 1.05227705967608]
+        ωₙ_ans = [-0.0853652197846037, -0.0371827945713394,0.12254801435943]
+        @test all(sort(ωₙ_ans) .≈ sort(ωₙ))
+        @test all(sort(Γₙ_ans) .≈ sort(Γₙ))
+    
+        R_nm = get_pairwise_matrix(atoms.r)
+        R_nm_ans = [0 1.7320508075688 1.5; 
+                    1.73205080756888 0 2.06155281280883; 
+                    1.5 2.06155281280883 0]
+        @test all(R_nm_ans .≈ R_nm)
+    
+        G = get_interaction_matrix(problem)
+        G_ans = [-0.5 -0.284930049591257-0.04634868038121*im -0.332498328868018+0.0235790672225676*im;
+                 -0.284930049591257-0.046348680381261*im -0.5 -0.213910742134572-0.11430539712927*im;
+                 -0.332498328868018+0.0235790672225676*im -0.213910742134572-0.11430539712197*im -0.5]
+        @test all(G .≈ G_ans)
+    end
 end
